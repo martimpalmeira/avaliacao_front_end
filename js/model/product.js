764a07db18345a7c2b50d9626f8840fc0ae8e511
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-10-28 09:55:18 -0300

    Final

--- js/model/product.js
+++ js/model/product.js
@@ -1,9 +1,11 @@
 export default class Product{
-    constructor(name, price, description, linkImg, segment){
+    constructor(name, price, description, linkImg, segment, quantidadeNoCarrinho, estoque){
         this.name = name;
         this.price = price;
         this.description = description;
         this.linkImg = linkImg;
         this.segment = segment;
+        this.quantidadeNoCarrinho = quantidadeNoCarrinho
+        this.estoque = estoque
     }
 }
